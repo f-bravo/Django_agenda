72c48b35fa010cc8a195152ef7962b43809dce96
f-bravo <bravoback@outlook.com>
2023-06-16 12:20:46 -0300

    intro1

--- intro.py
+++ intro.py
@@ -1,6 +1,69 @@
+# Comandos:
 
+# ------------------------------------------------------
 
+"""
+# Iniciando o projeto: 
+# Criando o ambiente virtual:
+-> python -m venv venv
+# Ativando oambiente virtual no Windows 
+-> . venv\Scripts\Activate
+# instalando o django
+-> pip install django
+# Atualizar o pip
+-> pip install --upgrade pip
+# Startando o project
+-> django-admin startproject project .
+Teste para ver se o django está ok. Se n tiver feche e abra o VSCode
+-> python manage.py runserver
+"""
 
+#--------------------------------------------------------
+# Configurando o git ignore --> arquivos que não vão para o respositório
+"""
+crei o .gitignore na raiz do projeto 
+busque no google --> django gitignore 
+https://djangowaves.com/tips-tricks/gitignore-for-a-django-project/
+Cole os comandos dentro da pasta .gitignore
+"""
+
+# Baixe o GIT - aplique as opção default
+
+"""
+OBS: usar o mesmo nome e email do GITHUB
+# Criando nome de usuário:
+-> git config --global user.name 'nome'
+# Configurando email - outlook.com
+-> git config --global user.email 'email@email.com'
+# Olhando as configurações do GIT:
+-> git config --global
+
+# Eu não alterei 0 defaultBranch para main - deixei como master
+# estava dando erro na horade dar o push
+
+# Adicioanando:
+-> git add .  - para adicionar tudo
+
+# Dando o primeiro Commit
+-> git commit -m 'Initial'
+
+# dando um git log - para ver o que foi feito:
+# ou git log --oneline
+# R: c5ef3d1 (HEAD -> master) Initial
+
+# Crei o repositório no perfil do github
+Adicionando o repositório remoto: 
+copie a chave https e dê o seguinte comando:
+-> git remote add origin https://github.com/f-bravo/Django_agenda.git
+
+# Enviando os commites:
+-> git push origin master
+
+# digite usuário e senha e pronto - será enviado
+
+#
+
+"""
 
 
 
