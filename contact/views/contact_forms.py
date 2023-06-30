cdede52f15d873059b04577f7fa1bad09669a6a0
f-bravo <bravoback@outlook.com>
2023-06-30 11:01:37 -0300

    Add login_required em user_forms e contat_forms p/ forçar user está logado

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -2,8 +2,10 @@ from django.shortcuts import get_object_or_404, render, redirect
 from contact.forms import ContactForm
 from django.urls import reverse
 from contact.models import Contact
+from django.contrib.auth.decorators import login_required
 
 
+@login_required(login_url='contact:login')
 def create(request):
     form_action = reverse('contact:create')
 
@@ -37,7 +39,7 @@ def create(request):
             context
     )   
 
-
+@login_required(login_url='contact:login')
 def update(request, contact_id):
     contact = get_object_or_404(Contact, pk=contact_id, show=True)
     form_action = reverse('contact:update', args=(contact_id,))
@@ -71,6 +73,8 @@ def update(request, contact_id):
             context
     ) 
 
+
+@login_required(login_url='contact:login')
 def delete(request, contact_id):
     contact = get_object_or_404(
         Contact, pk=contact_id, show=True
